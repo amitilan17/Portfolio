d4a4db912faa9d072b7a8775cfcede2f2e0f136c
amitilan17 <amit.ilan17@gmail.com>
2024-12-05 16:23:05 +0100

    Todo cleanups

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -23,7 +23,6 @@ const generateProjectImages = (projectId, count) => {
 const projectsDataMap = {
     '1-2': {
         index: 1,
-        name: 'צלקת', // todo rename property
         thumbnailPath: p1Thumbnail,
         hebDescription: 'קולאז׳ וידאו בשני רבדים חיצוניים: טקסט ודימוי, ובשני רבדים פנימיים: אישי וקולקטיבי. דרך ההקבלה וההפרדה בין הקטן, הפרטי, הספציפי לבין הגדול, הגלובלי, המונומנטלי, הסרט הקצר מנסה לענות על השאלה:<strong>האם אפשר לתפוס את קץ האנושות?</strong>',
         engDescription: 'Video collage in two external layers: text and image, and in two internal layers: the personal and the collective. Through the parallelism and separation between the small, private, specific aspects and the large, global, monumental aspects, the short film tries to answer the question: <strong>Is it possible to grasp the end of humanity?</strong>',
@@ -36,7 +35,6 @@ const projectsDataMap = {
     },
     '1-3': {
         index: 2,
-        name: 'אז מה אם אתן תאומות',
         thumbnailPath: p2Thumbnail,
         hebDescription: 'יש לי שתי אחיות, נעם וניצן. הן תאומות, הן קטנות ממני בשנתיים, והן גדלו במסגרות נפרדות מגיל קטן, כך שבעצם אנחנו סוג של שלישיה. או אולי, משולש: כל אחת חולקת תכונות עם השניה והשלישית. למרות שאני אחותן הגדולה, לפעמים אני מרגישה דווקא באמצע בין ניצן לנעם. <strong>אז מה אם אתן תאומות</strong> הוא פרוייקט אינפוגרפי שמהווה דיוקן שלי דרכן, ודיוקן שלהן- דרכי. במסגרת הפרוייקט ריאיינתי אותן לעומק ובניתי ״דאטה-בייס״, ״ניתחתי״ את המידע ועיצבתי מערכת אינפוגרפית של 7 גלויות.',
         engDescription: 'I have two sisters, Noam and Nitzan. They are twins, two years younger than me, and grew up in separate settings from an early age, making us essentially a trio. Or perhaps, a triangle: each of us shares characteristics with the other two. Despite being their older sister, I sometimes feel I\'m positioned right between Nitzan and Noam. <strong>You\'re Twins, So What</strong> is an infographic project that creates a portrait of me through them, and them through me. For this project, I conducted in-depth interviews, built a "database," "analyzed" the data, and designed an infographic system of 7 postcards.',
@@ -47,7 +45,8 @@ const projectsDataMap = {
         images: generateProjectImages(2, 5)
     },
     '1-4': {
-        index: 3, name: 'רגעי חולין', thumbnailPath: p3Thumbnail,
+        index: 3,
+        thumbnailPath: p3Thumbnail,
         hebDescription: '<strong>רגעי חולין</strong> הוא מוצר אינטראקטיבי המזמין את המשתמש לגשר מעט על הפער בין חוויה לזכרון, דרך כתיבה ברגעים אקראיים. במסגרת הפרוייקט עיצבתי את המטרות, ההגדרות והמיתוג של המוצר, את האפליקציה שדרכה מתבצע איסוף הרגעים, ואת היומן המודפס שנשלח אל המשתמש בתום הכתיבה ומשמש כקפסולת זמן האוצרת בתוכה הוויה של תקופה.' +
             '<br/><br/>' +
             'במסגרת הפרוייקט פיתחתי אפליקציית אנדרואיד שאיפשרה לחוות את המוצר. מבחינת backend, העיבוד של המידע מהיוזר לפורמט מעוצב של עמוד ביומן נעשה בצורה אוטומטית בפייתון. כמו כן, פיתחתי שתי תכניות ששירתו שתי פרזנטציות שונות של הפרוייקט: האחת שולחת להדפסה במדפסת סמוכה את הרגע שזה עתה נכתב באפליקציה, כדי לייצר ״אפקט וואו״. השניה שולחת את אותו הקובץ למייל כעבור מספר ימים, ומטרתה לייצר את האפקט של קפסולת זמן.<br/><br/> ניתן לצפות בrepo של הפרוייקט בגיטהאב.',
@@ -74,7 +73,8 @@ const projectsDataMap = {
         cinematicPopup: true
     },
     '2-4': {
-        index: 5, name: 'Trigger it', thumbnailPath: p5Thumbnail,
+        index: 5,
+        thumbnailPath: p5Thumbnail,
         hebDescription: 'אפליקציית ליצירת אפקטים מבוססת תכנות. מטרתה לפתוח לבני נוער שער לתחום התכנות דרך עולם התוכן שלהם ומטרות רלוונטיות עבורם. <br/><br/>האפליקציה מבוססת על קומפוננטות מודולוריות ליצירת צירופים יחודיים ומקוריים, ומאפשרת שילוב של אופציות pre-defined ואופציות custom ליצירת ״רמות קושי״ שונות. <br/><br/>מתוך הבנת החשיבות של אוריינות דיגיטלית בכלל ויכולות תכנות בסיסיות בפרט בעולם המתפתח, האפליקציה שמה דגש על תהליך הלמידה ומעודדת מוטיבציה ללמידה מתוך יצירתיות, ביטוי אישי ואלמנטים של Project Based Learning' +
             '.',
         engDescription: 'A programming-based visual effects application that introduces teenagers to coding through their sphere of interests and relevant objectives. <br/><br/>Built on modular components, the application facilitates the creation of unique combinations through both pre-defined and custom options, accommodating various complexity levels. <br/><br/>Acknowledging the critical role of digital literacy and fundamental programming capabilities in our evolving digital landscape, the application emphasizes a comprehensive learning journey. It cultivates learning motivation through creative expression, individual articulation, and project-based learning methodology.',
@@ -85,31 +85,34 @@ const projectsDataMap = {
         images: generateProjectImages(5, 6)
     },
     '2-5': {
-        index: 6, name: 'המשביר לצרכן', thumbnailPath: p6Thumbnail,
+        index: 6,
+        thumbnailPath: p6Thumbnail,
         hebDescription: 'מעטפת גרפית למוסד חדש: ארכיון עירוני הנפתח בבניין <strong>המשביר לצרכן</strong> בירושלים. במסגרת הפרוייקט עיצבתי לוגו ופוסטרים לאירוע הפתיחה. השפה הגרפית שואבת השראה מצורת המבנה ומזוויות החלונות שלו, וקורצת גם למטרתו המקורית בשדה הצרכנות.',
         engDescription: 'A visual identity for a new institution: A municipal archive opening in the historic <strong>Hamashbir Lezarchan</strong> building in Jerusalem. As part of the project, I designed a logo and posters for the opening event. The graphic language draws inspiration from the building\'s structure and window angles, while giving a subtle nod to its original purpose in the retail sector.',
-        hebTitle: 'המשביר לצרכן',
-        engTitle: 'Hamashbir TBD', // todo translate
+        hebTitle: 'ארכיון המשביר',
+        engTitle: 'The Mashbir Archive',
         hebTags: [ "פרינט", "תרבותי", "מיתוג"],
-        engTags: ["Print", "Cultural", "Branding"], // todo brand or branding?
+        engTags: ["Print", "Cultural", "Branding"],
         images: generateProjectImages(6, 5)
     },
     '3-3': {
-        index: 7, name: 'נוסטלגיה וכו׳', thumbnailPath: p7Thumbnail,
+        index: 7,
+        thumbnailPath: p7Thumbnail,
         hebDescription: '<i>״אני מתגעגעת לתקופה שהיינו סטודנטים בירושלים״</i>, כתבתי בעודי סטודנטית בירושלים, בסוף השנה השלישית מתוך חמש.<br/> בפרוייקט זה עסקתי בנוסטלגיה ובחללים בהם אני חיה, בהשראת הספר<strong> חלל וכו׳</strong> מאת ג׳ורג׳ פרק ובצירוף קטעים ממנו. צילמתי במצלמת פילם את הבית שבו גרתי ושאותו, יחד עם מה שהוא ייצג עבורי, אהבתי מאוד, והרגשתי שאני כבר מתגעגעת אליו עוד כשגרתי שם. הבחירה בפילם באופן טבעי הדגישה את הנופך הנוסטלגי של ההתבוננות שלי בבית. הקולאז׳ של קטעים מהספר יחד עם הצילומים מבטא את מחשבותיי והרגשותיי לגבי האובזרבציות של פרק.',
         engDescription: '<i>"I miss the time we were students in Jerusalem,"</i> I wrote while still being a student in Jerusalem, at the end of my third year out of five. This project explores nostalgia and the spaces I inhabit, inspired by Georges Perec\'s <strong>Species of Spaces</strong> and incorporating passages from it. <br/><br/>Using an analog film camera, I captured the house where I lived—a place I deeply cherished, along with everything it represented—feeling nostalgic for it even while still residing there. The choice of analog photography naturally emphasized the nostalgic quality of my observation of the house. The collage of text passages combined with the photographs expresses my thoughts and feelings about Perec\'s observations.',
         hebTitle: 'נוסטלגיה וכו׳',
-        engTitle: 'Nostalgia TBD', // todo translate
+        engTitle: 'Nostalgia and Other Pieces',
         hebTags: [ "קולאז׳", "צילום", "אישי"],
         engTags: ["Collage", "Photography","Personal"],
         images: generateProjectImages(7, 10)
     },
     '3-4': {
-        index: 8, name: 'אופנת הקיבוץ', thumbnailPath: p8Thumbnail,
+        index: 8,
+        thumbnailPath: p8Thumbnail,
         hebDescription: 'בפרוייקט זה ערכתי את הערך <strong>אופנה בקיבוץ</strong> על פי ויקיפדיה יחד עם אוסף תצלומים משפחתי משנות ה70, שנות ילדותו של אבי בקיבוץ ראש הנקרה, למעין קטלוג שמציג את האופנה וחיי הקיבוץ של התקופה. או כמו שמתאר זאת היטב יורם קניוק, שגם מצוטט בסוף החוברת: <i>"אופנה איננה רק תצוגת בגדי ים... היא האני הקולקטיבי העליון של הגוף, של החברה, זה איך שאנשים היו רוצים להיות גזורים בתוך הנוף״</i>.',
         engDescription: 'In this project, I edited the Wikipedia article <strong>Fashion in the Kibbutz</strong> alongside a collection of family photographs from the 1970s, documenting my father\'s childhood in Kibbutz Rosh HaNikra, into a fashion catalog that captures kibbutz fashion and life of that era. As Yoram Kaniuk aptly describes, and as quoted at the end of the booklet: <i>"Fashion is not merely a swimwear show... it is the collective higher self of the body, of society, it is how people wish to be carved into the landscape."</i>',
         hebTitle: 'אופנת הקיבוץ',
-        engTitle: 'The Kibbutz Fashion', // todo translate
+        engTitle: 'Kibbutz Fashion',
         hebTags: [ "תרבותי", "פרינט", "אישי"],
         engTags: ["Cultural", "Print", "Personal"],
         images: generateProjectImages(8, 10)
